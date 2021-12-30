36aafc1e35fbf955f90e6090ef188840c0cb4cd1
marcusruas <marcus.ruas@hotmail.com>
2021-12-30 00:41:17 -0300

    Adicionado configurações de API

--- Api/Startup.cs
+++ Api/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,28 +23,51 @@ namespace Api
 
         public IConfiguration Configuration { get; }
 
+        private const string NOME_API = "Scaffold API";
+        private const string VERSAO_API = "v1";
+        private const string NOME_POLITICA_CORS = "Autorização Front-end";
+        private string[] ORIGENS_CORS = new string[] { "http://localhost:4200", "https://localhost:4200" };
+
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddCors(options => {
+                options.AddPolicy(
+                    NOME_POLITICA_CORS,
+                    builder => builder
+                        .WithOrigins(ORIGENS_CORS)
+                        .SetIsOriginAllowedToAllowWildcardSubdomains()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()
+                        .AllowCredentials()
+                );
+            });
+
+            services.AddSwaggerGen(options =>
+                options.SwaggerDoc(VERSAO_API, new OpenApiInfo
+                {
+                    Version = VERSAO_API,
+                    Title = NOME_API
+                })
+            );
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
-            {
                 app.UseDeveloperExceptionPage();
-            }
 
             app.UseHttpsRedirection();
-
             app.UseRouting();
-
             app.UseAuthorization();
-
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
             });
+
+            app.UseSwagger();
+            app.UseSwaggerUI();
         }
     }
 }
---
+++ README.md
@@ -0,0 +1,1 @@
+# Scaffold de API
